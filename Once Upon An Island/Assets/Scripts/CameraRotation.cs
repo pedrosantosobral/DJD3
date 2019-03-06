c727ce4b4a562c43cc1603da1f04abcc4edc3e08
Pedro Santos <pedrosantosobral@gmail.com>
2019-03-06 16:22:18 +0000

    add inicial game prototype

---
+++ Once Upon An Island/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    private float _mouseSpeed = 2.5f;
+
+    void Update()
+    {
+        RotateCamera();
+    } 
+
+    private void RotateCamera()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * _mouseSpeed, 0);
+        }
+
+    }
+}
---
+++ Once Upon An Island/Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float movimentSpeed;
+    private CharacterController player;
+
+    private Vector3 _guideVector;
+    private Vector3 _movimentVector;
+    private GameObject _guide;
+
+    void Start()
+    {
+        _guide = GameObject.Find("CameraRotation_guide");
+        player = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        UpdateGuide();
+        UpdateMovimentVector();
+        UpdatePlayerMovimentItself();
+    }
+
+    private void UpdateGuide()
+    {
+        _guideVector = _guide.transform.position;
+        _movimentVector.x = _guideVector.x;
+        _movimentVector.y = _guideVector.y;
+    }
+
+    private void UpdateMovimentVector()
+    {
+        _movimentVector.x =  Input.GetAxis("Horizontal") * movimentSpeed;
+        _movimentVector.y = 0;
+        _movimentVector.z = Input.GetAxis("Vertical") * movimentSpeed;
+    }
+
+    private void UpdatePlayerMovimentItself()
+    {
+        player.Move(transform.TransformVector(_movimentVector));
+    }
+
+
+}
